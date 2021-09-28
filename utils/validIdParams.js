2ad5d33eea7a4ac85e5121b502cf5162a436cf25
Nikita Sotnikov <nsotnikov16@yandex.ru>
2021-09-28 08:15:18 +0500

    edit function

--- utils/validIdParams.js
+++ utils/validIdParams.js
@@ -2,17 +2,10 @@ const { Joi } = require('celebrate');
 
 module.exports = (typeId) => {
   const validation = Joi.string().hex().length(24).required();
-  if (typeId === 'cardId') {
-    return {
-      params: Joi.object().keys({
-        cardId: validation,
-      }),
-    };
-  }
 
   return {
     params: Joi.object().keys({
-      userId: validation,
+      [typeId]: validation,
     }),
   };
 };
